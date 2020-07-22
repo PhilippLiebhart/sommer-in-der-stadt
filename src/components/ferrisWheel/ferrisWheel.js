93353dbf578a764620172727cc5f088934ba77a6
Philos <philipp@philipp-liebhart.de>
2020-07-22 17:41:05 +0200

    ferriswheel

---
+++ src/components/ferrisWheel/ferrisWheel.js
@@ -0,0 +1,48 @@
+import React from "react"
+
+import "./FerrisWheel.css"
+
+const FerrisWheel = () => {
+  return (
+    <div class="ferris_wheel">
+      <div class="wheel_wrap">
+        <div class="wheel">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div class="buckets">
+        <div>
+          <span></span>
+        </div>
+        <div>
+          <span></span>
+        </div>
+        <div>
+          <span></span>
+        </div>
+        <div>
+          <span></span>
+        </div>
+        <div>
+          <span></span>
+        </div>
+        <div>
+          <span></span>
+        </div>
+        <div>
+          <span></span>
+        </div>
+        <div>
+          <span></span>
+        </div>
+      </div>
+      <div class="stand">
+        <span></span>
+      </div>
+    </div>
+  )
+}
+
+export default FerrisWheel
--- src/pages/attractions.js
+++ src/pages/attractions.js
@@ -1,13 +1,19 @@
 import React from "react"
 import Layout from "../components/layout"
 
+import EventCardSimple from "../components/eventCardSimple/eventCardSimple"
+
+import "./attractions.css"
+import FerrisWheel from "../components/ferrisWheel/ferrisWheel"
+
 const AttractionsPage = ({ pageContext }) => {
-  console.log("page context", pageContext.name)
   return (
     <Layout>
       <div className="attractionsWrapper">
         <h1>Attractions page</h1>
         <p>Riesenrad, etc etc</p>
+        <FerrisWheel />
+        <EventCardSimple />
       </div>
     </Layout>
   )
