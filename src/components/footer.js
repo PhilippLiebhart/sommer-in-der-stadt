6c677f053984ecddbb5edbabbf8bdb9ce1f8892f
Barry Norman <git@barrynorman.de>
2020-08-07 15:53:02 +0200

    logo sponsor

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,37 +7,61 @@ const Footer = () => {
         className="section mt-5 mb-0 pt-5 pb-3"
         style={{ backgroundColor: "white" }}
       >
-        <div className="columns is-multiline is-vcentered is-mobile is-desktop is-tablet">
-          <div className="column is-half-tablet is-full-mobile is-one-quarter-desktop">
-            <img
-              style={{ height: "auto", width: "100%" }}
-              src={`https://cdn.shortpixel.ai/client/to_webp,q_lossless,ret_img,h_89/https://die-stickmeister.de/wp-content/uploads/2020/06/Stickmeisters_Logo_Solo_LightGray.png`}
-              alt=""
-            />
+        <div className="columns is-multiline is-vcentered is-centered is-mobile is-desktop is-tablet">
+          <div className="column is-narrow is-half-tablet is-full-mobile is-one-quarter-desktop">
+            <a
+              target="_blank"
+              href="https://die-stickmeister.de"
+              className="sponsor-link"
+            >
+              <img
+                className="sponsor-logo"
+                src={`https://cdn.shortpixel.ai/client/to_webp,q_lossless,ret_img,h_89/https://die-stickmeister.de/wp-content/uploads/2020/06/Stickmeisters_Logo_Solo_LightGray.png`}
+                alt=""
+              />
+            </a>
           </div>
 
-          <div className="column is-half-tablet is-full-mobile is-one-quarter-desktop">
-            <img
-              style={{ height: "auto", width: "100%" }}
-              src={`https://cdn.shortpixel.ai/client/to_webp,q_lossless,ret_img,h_89/https://die-stickmeister.de/wp-content/uploads/2020/06/Stickmeisters_Logo_Solo_LightGray.png`}
-              alt=""
-            />
+          <div className="column is-narrow is-half-tablet is-full-mobile is-one-quarter-desktop">
+            <a
+              target="_blank"
+              href="https://die-stickmeister.de"
+              className="sponsor-link"
+            >
+              <img
+                className="sponsor-logo"
+                src={`https://www.hausverwaltung-norman.de/wp-content/uploads/2020/03/Hausverw_Logo_Final.png`}
+                alt=""
+              />
+            </a>
           </div>
 
-          <div className="column is-half-tablet is-full-mobile is-one-quarter-desktop">
-            <img
-              style={{ height: "auto", width: "100%" }}
-              src={`https://cdn.shortpixel.ai/client/to_webp,q_lossless,ret_img,h_89/https://die-stickmeister.de/wp-content/uploads/2020/06/Stickmeisters_Logo_Solo_LightGray.png`}
-              alt=""
-            />
+          <div className="column is-narrow is-half-tablet is-full-mobile is-one-quarter-desktop">
+            <a
+              target="_blank"
+              href="https://die-stickmeister.de"
+              className="sponsor-link"
+            >
+              <img
+                className="sponsor-logo"
+                src={`https://cdn.shortpixel.ai/client/to_webp,q_lossless,ret_img,h_89/https://die-stickmeister.de/wp-content/uploads/2020/06/Stickmeisters_Logo_Solo_LightGray.png`}
+                alt=""
+              />
+            </a>
           </div>
 
-          <div className="column is-half-tablet is-full-mobile is-one-quarter-desktop">
-            <img
-              style={{ height: "auto", width: "100%" }}
-              src={`https://cdn.shortpixel.ai/client/to_webp,q_lossless,ret_img,h_89/https://die-stickmeister.de/wp-content/uploads/2020/06/Stickmeisters_Logo_Solo_LightGray.png`}
-              alt=""
-            />
+          <div className="column is-narrow is-half-tablet is-full-mobile is-one-quarter-desktop">
+            <a
+              target="_blank"
+              href="https://die-stickmeister.de"
+              className="sponsor-link"
+            >
+              <img
+                className="sponsor-logo"
+                src={`https://cdn.shortpixel.ai/client/to_webp,q_lossless,ret_img,h_89/https://die-stickmeister.de/wp-content/uploads/2020/06/Stickmeisters_Logo_Solo_LightGray.png`}
+                alt=""
+              />
+            </a>
           </div>
         </div>
       </div>
